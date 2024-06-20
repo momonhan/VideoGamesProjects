6df936cf1373cb4af1a39dd3cfe905b60fa7ceca
momonhan <372291nhan@gmail.com>
2024-06-20 14:28:37 -0500

    fixed bug

--- videogame.py
+++ videogame.py
@@ -1,6 +1,13 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-vg= pd.read_csv('vgsales.csv')
-vg
-vg.head(10)
+import seaborn as sns
+from matplotlib import pyplot as plt
+plt.style.use('fivethirtyeight')
+csv_reader = pd.read_csv(r'C:\Users\37229\OneDrive\Desktop\Projects\top10.csv')
+sns.barplot(x = 'Global_Sales', y = 'Name', data = csv_reader, palette = "Blues_d")
+plt.xlabel('Global Sales (in millions)')
+plt.ylabel('Video Games')
+plt.title('Top 10 Video Games by Global Sales')
+plt.tight_layout()
+plt.savefig('VideoGame.png')
+plt.show()
